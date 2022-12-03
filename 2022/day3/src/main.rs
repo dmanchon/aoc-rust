9bfe0068996e7f67e14fabfb7b65e2db869f487b
Daniel Manchon <dmanchon@gmail.com>
2022-12-03 19:37:34 +0100

    day3: performance

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,80 +1,55 @@
 use std::collections::HashSet;
 
-fn calc_score(item: char) -> u32 {
-    if item.is_lowercase() {
-        item as u32 - '`' as u32
+fn calc_score(item: u8) -> u16 {
+    if item.is_ascii_lowercase() {
+        item as u16 - '`' as u16
     } else {
-        item as u32 - '@' as u32 + 26
+        item as u16 - '@' as u16 + 26
     }
 }
 
+fn get_common(first: &[u8], second: &[u8], third: &[u8]) -> u8 {
+    for ch in first {
+        if second.contains(ch) && third.contains(ch) {
+            return *ch;
+        }
+    };
+    panic!()
+}
+
+fn part2() {
+    let mut solution: u16 = 0;
+    let mut lines = vec![];
+    for line in include_str!("../input.txt").lines() {
+        lines.push(line);
+    }
+    
+    for chunk in lines.chunks(3) {
+        let first = chunk[0].as_bytes();
+        let second = chunk[1].as_bytes();
+        let third = chunk[2].as_bytes();
+
+        solution += calc_score(get_common(first, second, third));
+       
+    }
+    println!("part2: {:#?}", solution);
+}
+
 fn part1() {
-    let mut solution: u32 = 0;
+    let mut solution: u16 = 0;
     for line in include_str!("../input.txt").lines() {
         let (l, r) = line.split_at(line.len() / 2);
         let a: HashSet<char> = HashSet::from_iter(l.chars());
         let b: HashSet<char> = HashSet::from_iter(r.chars());
 
         if let Some(&c) = a.intersection(&b).next() {
-            solution += calc_score(c);
+            solution += calc_score(c as u8);
         };
     }
     println!("part1: {:#?}", solution);
 }
 
-fn part2() {
-    let mut solution: u32 = 0;
-    let mut lines = vec![];
-    for line in include_str!("../input.txt").lines() {
-        lines.push(line);
-    }
-
-    for chunk in lines.chunks(3) {
-        let mut first: HashSet<char> = HashSet::from_iter(chunk[0].chars());
-        let second: HashSet<char> = HashSet::from_iter(chunk[1].chars());
-        let third: HashSet<char> = HashSet::from_iter(chunk[2].chars());
-
-        first.retain(|element| {
-            for s in [&second, &third] {
-                if !s.contains(element) {
-                    return false;
-                }
-            }
-            true
-        });
-
-        if let Some(&c) = first.iter().next() {
-            solution += calc_score(c);
-        };
-    }
-    println!("part2: {:#?}", solution);
-}
-
-fn part2_idiomatic() {
-    // sort of idiomatic, still trying to learn Rust
-    // is this really more idiomatic? or readable?
-    let solution: u32 = include_str!("../input.txt")
-        .lines()
-        .collect::<Vec<_>>()
-        .chunks(3)
-        .map(|chunk| {
-            chunk
-                .iter()
-                .map(|c| c.chars())
-                .map(|x| HashSet::<char>::from_iter(x))
-                .reduce(|mut a, b| {
-                    a.retain(|x| b.contains(x));
-                    a
-                })
-                .map_or(0, |x| calc_score(*x.iter().next().unwrap()))
-        })
-        .sum();
-
-    println!("part2: {:#?}", solution);
-}
-
 fn main() {
     part1();
     part2();
-    part2_idiomatic()
 }
