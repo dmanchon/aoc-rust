1929240cf10fec1bdf5c75b261658a50ac679c73
Daniel Manchon <dmanchon@gmail.com>
2022-12-05 16:23:48 +0100

    day5

--- 2022/day5/src/main.rs
+++ 2022/day5/src/main.rs
@@ -1,4 +1,4 @@
-fn part1() {
+fn parse() -> (Vec<Vec<char>>, &'static str) {
     let input: Vec<&str> = include_str!("../input.txt").split("\n\n").collect();
 
     let state: Vec<_> = input[0].lines().collect();
@@ -18,8 +18,25 @@ fn part1() {
         tmp.reverse();
         stacks[stack_no] = tmp;
     }
+    for stack_no in 0..N {
+        let mut tmp = vec![];
+        for line_no in 0..tallest {
+            let index = stack_no*4 + 1;
+            let c = state[line_no].as_bytes()[index];
+            if c != b' ' {
+                tmp.push(c as char);
+            }
+        }
+        tmp.reverse();
+        stacks[stack_no] = tmp;
+    }
 
-    for move_str in input[1].lines() {
+    (stacks, input[1])
+}
+
+fn part1() {
+    let (mut stacks, moves) = parse();
+    for move_str in moves.lines() {
         let mov: Vec<_> = move_str.split(r" ").collect();
         let (n, from, to) = (mov[1].parse::<usize>().unwrap(), mov[3].parse::<usize>().unwrap(), mov[5].parse::<usize>().unwrap());
 
@@ -39,39 +56,13 @@ fn part1() {
 }
 
 fn part2() {
-    let input: Vec<&str> = include_str!("../input.txt").split("\n\n").collect();
-
-    let state: Vec<_> = input[0].lines().collect();
-    let tallest = state.len() - 1;
-    const N: usize = 9;
-    let mut stacks: Vec<Vec<char>> = vec![vec![]; N];
-
-    for stack_no in 0..N {
-        let mut tmp = vec![];
-        for line_no in 0..tallest {
-            let index = stack_no*4 + 1;
-            let c = state[line_no].as_bytes()[index];
-            if c != b' ' {
-                tmp.push(c as char);
-            }
-        }
-        tmp.reverse();
-        stacks[stack_no] = tmp;
-    }
-
-    for move_str in input[1].lines() {
+    let (mut stacks, moves) = parse();
+    for move_str in moves.lines() {
         let mov: Vec<_> = move_str.split(r" ").collect();
         let (n, from, to) = (mov[1].parse::<usize>().unwrap(), mov[3].parse::<usize>().unwrap(), mov[5].parse::<usize>().unwrap());
 
-        //i am sure this can be improved
-        let mut block: Vec<char> = vec![];
-        for _ in 0..n {
-            if let Some(e) = stacks[from-1].pop() {
-                block.push(e);                
-            } 
-        }
-        block.reverse();
-        for e in block {
+        let len = stacks[from-1].len();
+        for e in stacks[from-1].drain(len-n..).collect::<Vec<_>>() {
             stacks[to-1].push(e);            
         }
 
