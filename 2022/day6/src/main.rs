16820b899561f5e2d5b53bdab8f1539fb54fd22e
Daniel Manchon <dmanchon@gmail.com>
2022-12-06 16:17:47 +0100

    day6

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -1,9 +1,20 @@
 use std::collections::{HashSet, VecDeque};
 
+fn solve_idiomatic(n: usize) -> usize {
+    if let Some(pos) = include_bytes!("../input.txt")
+        .windows(n)
+        .map(|w| w.iter().collect::<HashSet<_>>().len())
+        .position(|x| x == n) {
+            pos + n
+        } else {
+            0
+        }
+}
+
 fn solve(n: usize) -> usize {
     let mut header: VecDeque<&u8> = VecDeque::new();
     let mut solution: usize = 0;
-    
+
     for c in include_bytes!("../input.txt") {
         solution += 1;
         header.push_back(c);
@@ -20,4 +31,6 @@ fn solve(n: usize) -> usize {
 fn main() {
     println!("part1: {:#?}", solve(4));
     println!("part2: {:#?}", solve(14));
+    println!("part1 (idiomatic): {:#?}", solve_idiomatic(4));
+    println!("part2 (idiomatic): {:#?}", solve_idiomatic(14));
 }
